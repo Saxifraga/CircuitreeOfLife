97f3d6b4ae4acd5b3302bf4a74713db8bd64a509
Rachel Sassella <sassella@dhcp-130-58-103-83.swarthmore.edu>
2017-01-26 09:53:05 -0500

    trying to read .raw files

--- openspice.py
+++ openspice.py
@@ -5,5 +5,7 @@ import subprocess as sp
 
 spicepath = r'/Applications/ngspice/bin/ngspice'
 
-p = sp.Popen(["%s" % (spicepath), "-b", "-r", "rawfile.raw", "xspice_c1.cir"])
-#p.communicate()
+#p = sp.Popen(["%s" % (spicepath), "-b", "-r", "rawfile.raw", "xspice_c1.cir"])
+p = sp.Popen(["%s" % (spicepath), "-s", "parameter_sweep.cir"])
+
+p.communicate() #this line causes to ngspice to close after it's done simulating
